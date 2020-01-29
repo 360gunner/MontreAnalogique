aea37dafb65b32eb7bd8e38a51124407e9d3868d
Houssem Eddine <46385752+360gunner@users.noreply.github.com>
2020-01-29 12:28:51 +0100

    better styles

--- src/app/canvas-clock/canvas-clock.component.ts
+++ src/app/canvas-clock/canvas-clock.component.ts
@@ -59,15 +59,15 @@ export class CanvasClockComponent implements OnInit, OnDestroy, AfterViewInit {
     ctx.fillStyle = 'white';
     ctx.fill();
     const grad = ctx.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
-    grad.addColorStop(0, '#333');
-    grad.addColorStop(0.5, 'white');
-    grad.addColorStop(1, '#333');
+    grad.addColorStop(0, 'grey');
+    grad.addColorStop(0.5, 'black');
+    grad.addColorStop(1, '#725');
     ctx.strokeStyle = grad;
     ctx.lineWidth = radius * 0.1;
     ctx.stroke();
     ctx.beginPath();
     ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
-    ctx.fillStyle = '#333';
+    ctx.fillStyle = '#111';
     ctx.fill();
   }
 
