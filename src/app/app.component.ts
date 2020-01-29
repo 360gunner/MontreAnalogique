a6581ee90fb9d2c59bdcb03d5ffbcb3d8ff1deec
Houssem Eddine <46385752+360gunner@users.noreply.github.com>
2020-01-29 12:31:23 +0100

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'cs-root',
-  template: `<h1>{{ title }}</h1> <h1>{{ subtitle }}</h1> <cs-canvas-clock></cs-canvas-clock>`,
+  template: `<h1></h1> <h1></h1> <cs-canvas-clock></cs-canvas-clock>`,
   styles: [`:host { display: grid; justify-items: center; }`]
 })
 export class AppComponent {
-  title = 'Angular Canvas Clock';
-  subtitle = 'Stackblitz = Awesome';
+ 
 }
